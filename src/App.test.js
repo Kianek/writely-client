df1b18460aca39e248e0131de73908b42942a6b7
Kianek <kallridge92@gmail.com>
2020-09-12 16:02:47 -0700

    Update spec

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const divElement = getByText(/My fancy app/i);
+  const { getByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const divElement = getByTestId('appComponent');
   expect(divElement).toBeInTheDocument();
 });
