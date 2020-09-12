c7eb30a2f7c4d1a7a6d82c06f32d2eb6f56b3c09
Kianek <kallridge92@gmail.com>
2020-09-12 16:02:15 -0700

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 
+import Navbar from './components/Navbar';
+import Page from './components/Page';
+import Footer from './components/Footer';
+import SignIn from './pages/SignIn';
+import Register from './pages/Register';
+import Settings from './pages/Settings';
+import Dashboard from './pages/Dashboard';
+import ChangePassword from './pages/ChangePassword';
+import JournalView from './pages/JournalView';
+
 function App() {
-  return <div className="App">My fancy app</div>;
+  return (
+    <div id="app" data-testid="appComponent">
+      <Navbar />
+      <Page>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route path="/register" component={Register} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/journal/:id" component={JournalView} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/settings/change-password" component={ChangePassword} />
+        </Switch>
+      </Page>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
