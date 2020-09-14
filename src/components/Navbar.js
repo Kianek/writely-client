a71f22999ef375c1da1c42e6377d849ca039bdef
Kianek <kallridge92@gmail.com>
2020-09-14 08:29:53 -0700

    Add dropdown menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ProfileButton from './ProfileButton';
 import Button from './Button';
+import Menu from './Menu';
 import '../styles/_layout.scss';
 
 function Navbar() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const toggleDropdown = () => setDropdownVisible(!dropdownVisible);
+
   return (
     <nav>
       <ul>
         <Button branding flat link to="/">
           Writely
         </Button>
-        <Button flat link to="/register">
-          Register
-        </Button>
+        {true ? (
+          <>
+            <ProfileButton toggled={dropdownVisible} onClick={toggleDropdown} />
+            <Menu showMenu={dropdownVisible}>
+              <Button block flat link to="/settings">
+                Settings
+              </Button>
+              <Button block flat link to="/">
+                Sign Out
+              </Button>
+            </Menu>
+          </>
+        ) : (
+          <Button flat link to="/register">
+            Register
+          </Button>
+        )}
       </ul>
     </nav>
   );
