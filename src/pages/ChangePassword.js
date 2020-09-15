6b9575f04bcc6991325d8a9391e340918fb4edba
Kianek <kallridge92@gmail.com>
2020-09-15 08:47:01 -0700

    Add main content

--- src/pages/ChangePassword.js
+++ src/pages/ChangePassword.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../components/Button';
+import Panel from '../components/Panel';
+import Form from '../components/Form';
+import TextInput from '../components/TextInput';
+import ToolBar from '../components/ToolBar';
 
 function ChangePassword() {
-  return <div>ChangePassword works!</div>;
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  function updatePassword() {
+    setNewPassword('');
+    setConfirmPassword('');
+  }
+
+  return (
+    <>
+      <ToolBar>
+        <Button link to="/settings">
+          <i className="fas fa-arrow-left" /> Settings
+        </Button>
+      </ToolBar>
+      <Panel>
+        <h3>Change Password</h3>
+        <Form>
+          <TextInput
+            block
+            placeholder="New Password"
+            value={newPassword}
+            onChange={setNewPassword}
+          />
+          <TextInput
+            block
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={setConfirmPassword}
+          />
+          <Button block flat onClick={updatePassword}>
+            Save
+          </Button>
+        </Form>
+      </Panel>
+    </>
+  );
 }
 
 export default ChangePassword;
