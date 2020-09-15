5e69980a096e0aa9a2764570d557fd3c27ca21bd
Kianek <kallridge92@gmail.com>
2020-09-15 08:47:20 -0700

    Add main content

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Modal from '../components/Modal';
+import HorizontalRule from '../components/HorizontalRule';
+import Panel from '../components/Panel';
+import ToolBar from '../components/ToolBar';
+import Button from '../components/Button';
 
 function Settings() {
-  return <div>Settings works!</div>;
+  function downloadUserData() {}
+
+  return (
+    <>
+      <ToolBar>
+        <Button link to="/dashboard">
+          <i className="fas fa-arrow-left" /> Dashboard
+        </Button>
+      </ToolBar>
+      <Panel>
+        <Button block flat link to="/change-password">
+          Change Password
+        </Button>
+        <Button block flat onClick={downloadUserData}>
+          Download Account Data
+        </Button>
+        <HorizontalRule />
+        <Button block flat>
+          Delete Account
+        </Button>
+      </Panel>
+    </>
+  );
 }
 
 export default Settings;
