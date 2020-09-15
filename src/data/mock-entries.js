cb00ad9b72b1f646ec1c9db3d1a2319bbe76b67e
Kianek <kallridge92@gmail.com>
2020-09-15 15:59:37 -0700

    Initial commit

---
+++ src/data/mock-entries.js
@@ -0,0 +1,12 @@
+const mockEntries = [
+  { id: 1, journalId: 1, title: 'Entry 1', createdAt: new Date().getDate() },
+  { id: 2, journalId: 1, title: 'Entry 2', createdAt: new Date().getDate() },
+  { id: 3, journalId: 1, title: 'Entry 3', createdAt: new Date().getDate() },
+  { id: 4, journalId: 1, title: 'Entry 4', createdAt: new Date().getDate() },
+  { id: 5, journalId: 1, title: 'Entry 5', createdAt: new Date().getDate() },
+  { id: 6, journalId: 1, title: 'Entry 6', createdAt: new Date().getDate() },
+  { id: 7, journalId: 1, title: 'Entry 7', createdAt: new Date().getDate() },
+  { id: 8, journalId: 1, title: 'Entry 8', createdAt: new Date().getDate() },
+];
+
+export default mockEntries;
---
+++ src/data/mock-journals.js
@@ -0,0 +1,15 @@
+const mockJournals = [
+  { id: 1, title: "Yo, I'm a Journal", entries: [{}] },
+  { id: 2, title: 'Me Too, lol' },
+  { id: 3, title: 'Same here' },
+  { id: 4, title: 'Yodely Doo' },
+  { id: 5, title: 'Writing Stuff' },
+  { id: 6, title: 'Read Me' },
+  { id: 7, title: 'Blibbity Bloo' },
+  { id: 8, title: 'Favorite Food' },
+  { id: 9, title: "Movies I've watched" },
+  { id: 10, title: 'Favorite Animals' },
+  { id: 11, title: 'Nostalgic TV Shows' },
+];
+
+export default mockJournals;
