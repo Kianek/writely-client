c850058b888385fb03fcf8a7b1c0932f86f09db8
Kianek <kallridge92@gmail.com>
2020-09-15 14:37:23 -0700

    Add styles & add'l config

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import classNames from 'classnames';
+import '../styles/_toolbar.scss';
 
-function ToolBar(props) {
-  return <div className="toolbar">{props.children}</div>;
+function ToolBar({ left, right, children }) {
+  const activeStyles = { 'justify-left': left, 'justify-right': right };
+
+  return (
+    <div className={classNames(activeStyles)} id="toolbar">
+      {children}
+    </div>
+  );
 }
 
 export default ToolBar;
