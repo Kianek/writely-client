1e98fe2af84a67f0000f04a8afc573267766f7e8
Kianek <kallridge92@gmail.com>
2020-09-15 17:27:56 -0700

    Add main content

--- src/pages/JournalView.js
+++ src/pages/JournalView.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Entry from '../components/Entry';
+import EntryList from '../components/EntryList';
+import EntryView from '../components/EntryView';
+import mockEntries from '../data/mock-entries';
+import '../styles/_journal-view.scss';
 
 function JournalView() {
-  return <div>JournalView works!</div>;
+  const [entries, setEntries] = useState(
+    mockEntries.map((entry) => <Entry entry={entry} />)
+  );
+
+  function loadEntry() {}
+
+  return (
+    <div id="journal-view">
+      <EntryList>{entries}</EntryList>
+      <EntryView journal="Fancy Journal" />
+    </div>
+  );
 }
 
 export default JournalView;
