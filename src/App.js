dae272660dc84808a56d136704448e97efd6fe53
Kianek <kallridge92@gmail.com>
2020-09-17 10:42:18 -0700

    Refactor theme object into separate file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { jsx, css } from '@emotion/core';
 import { ThemeProvider } from 'emotion-theming';
 import { Switch, Route } from 'react-router-dom';
+import theme from './theme';
 
 import Navbar from './components/Navbar';
 import Page from './components/Page';
@@ -12,38 +13,6 @@ import Settings from './pages/Settings';
 import Dashboard from './pages/Dashboard';
 import ChangePassword from './pages/ChangePassword';
 import JournalView from './pages/JournalView';
-
-const theme = {
-  colors: {
-    primary: '#ffe2f8',
-    lightGray: '#d0cfcf',
-    darkGray: '#565254',
-    success: '#4adb26',
-    info: '#0066ff',
-    danger: '#ff0000',
-    white: '#fff',
-    black: '#000',
-    shadow: 'rgba(0,0,0,0.2)',
-    boxShadow: {
-      boxShadow: '0 0 4px 3px rgba(0, 0, 0, 0.2)',
-    },
-  },
-  container: {
-    block: {
-      display: 'block',
-    },
-  },
-  flex: {
-    column: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    row: {
-      display: 'flex',
-      flexDirection: 'row',
-    },
-  },
-};
 
 const appStyles = (theme) => css`
   ${theme.flex.column}
