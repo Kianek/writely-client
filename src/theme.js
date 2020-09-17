0d7436c4c8a07e63539e41b23573162ad5b0e864
Kianek <kallridge92@gmail.com>
2020-09-17 10:42:28 -0700

    Initial commit

---
+++ src/theme.js
@@ -0,0 +1,33 @@
+const theme = {
+  colors: {
+    primary: '#ffe2f8',
+    lightGray: '#d0cfcf',
+    darkGray: '#565254',
+    success: '#4adb26',
+    info: '#0066ff',
+    danger: '#ff0000',
+    white: '#fff',
+    black: '#000',
+    shadow: 'rgba(0,0,0,0.2)',
+    boxShadow: {
+      boxShadow: '0 0 4px 3px rgba(0, 0, 0, 0.2)',
+    },
+  },
+  container: {
+    block: {
+      display: 'block',
+    },
+  },
+  flex: {
+    column: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    row: {
+      display: 'flex',
+      flexDirection: 'row',
+    },
+  },
+};
+
+export default theme;
