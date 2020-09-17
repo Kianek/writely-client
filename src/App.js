ecee73ae91365daed5d3efa666074b447e7a4cb6
Kianek <kallridge92@gmail.com>
2020-09-16 18:12:18 -0700

    Create color themes and add theme provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { ThemeProvider } from 'emotion-theming';
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 
@@ -12,32 +14,48 @@ import Dashboard from './pages/Dashboard';
 import ChangePassword from './pages/ChangePassword';
 import JournalView from './pages/JournalView';
 
+const theme = {
+  colors: {
+    primary: '#ffe2f8',
+    lightGray: '#d0cfcf',
+    darkGray: '#565254',
+    success: '#4adb26',
+    info: '#0066ff',
+    danger: '#ff0000',
+    white: '#fff',
+    black: '#000',
+    shadow: 'rgba(0,0,0,0.2)',
+  },
+};
+
 function App() {
   return (
-    <div id="app" data-testid="appComponent">
-      <Navbar />
-      <Page>
-        <Switch>
-          <Route exact path="/">
-            <SignIn />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/journal/:id">
-            <JournalView />
-          </Route>
-          <Route path="/settings">
-            <Settings />
-          </Route>
-          <Route exact path="/change-password" component={ChangePassword} />
-        </Switch>
-      </Page>
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div id="app" data-testid="appComponent">
+        <Navbar />
+        <Page>
+          <Switch>
+            <Route exact path="/">
+              <SignIn />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/journal/:id">
+              <JournalView />
+            </Route>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+            <Route exact path="/change-password" component={ChangePassword} />
+          </Switch>
+        </Page>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
