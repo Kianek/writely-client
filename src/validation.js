11851e04fff82e519b27cbae1868a4f0fa18d365
Kianek <kallridge92@gmail.com>
2020-09-19 13:54:42 -0700

    Fix bug in validationExecutor

--- src/validation.js
+++ src/validation.js
@@ -1,18 +1,26 @@
 import validator from 'validator';
 
 export const minLength = (min) => (val) =>
-  (!!val && val.length >= min) || `Must be at least ${min}`;
+  (!!val && val.length >= min) || `must be at least ${min}`;
 
 export const isValid = (val) =>
-  (!!val && !validator.isEmpty(val)) || `Must not be empty`;
+  (!!val && !validator.isEmpty(val)) || `must not be empty`;
 
 export const isEmail = (val) =>
-  (!!val && validator.isEmail(val)) || `Must be a valid email`;
+  (!!val && validator.isEmail(val)) || `must be a valid email`;
 
 export const containsSpecialChars = (val) =>
-  (!!val && validator.contains(val, '?!@#$%^&*|')) ||
-  `Must contain at least 1 non-letter and non-numeric character`;
+  (!!val && validator.matches(val, /[?!@#$%^&*|]/i)) ||
+  `must contain at least 1 non-letter and non-numeric character`;
 
 export const validationExecutor = (value, validationFunctions) => {
-  return validationFunctions.map((func) => func(value)).join('\n');
+  let errors = [];
+  validationFunctions.forEach((func) => {
+    const result = func(value);
+
+    if (typeof result === 'string') {
+      errors.push(result);
+    }
+  });
+  return errors.join(', ');
 };
