5d70b532e76ad72c0ae8cd3553590b1cf5f1c077
Kianek <kallridge92@gmail.com>
2020-09-20 12:00:31 -0700

    Change font color to dark gray

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import { jsx, css } from '@emotion/core';
 
 const styles = (theme) => css`
   background-color: ${theme.colors.primary};
+  color: ${theme.colors.darkGray};
   padding: 1rem;
   margin-top: auto;
   text-align: center;
