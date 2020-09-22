05ad4a29233f6aac1090e920f92c2c24fb12ceea
Kianek <kallridge92@gmail.com>
2020-09-22 15:57:47 -0700

    Update mock models

--- src/data/mock-journals.js
+++ src/data/mock-journals.js
@@ -1,16 +1,16 @@
 import entries from './mock-entries';
 const mockJournals = [
-  { id: 1, title: "Yo, I'm a Journal", entries },
-  { id: 2, title: 'Me Too, lol' },
-  { id: 3, title: 'Same here' },
-  { id: 4, title: 'Yodely Doo' },
-  { id: 5, title: 'Writing Stuff' },
-  { id: 6, title: 'Read Me' },
-  { id: 7, title: 'Blibbity Bloo' },
-  { id: 8, title: 'Favorite Food' },
-  { id: 9, title: "Movies I've watched" },
-  { id: 10, title: 'Favorite Animals' },
-  { id: 11, title: 'Nostalgic TV Shows' },
+  { id: 1, title: "Yo, I'm a Journal", entries, userId: 'UserId' },
+  { id: 2, title: 'Me Too, lol', userId: 'UserId' },
+  { id: 3, title: 'Same here', userId: 'UserId' },
+  { id: 4, title: 'Yodely Doo', userId: 'UserId' },
+  { id: 5, title: 'Writing Stuff', userId: 'UserId' },
+  { id: 6, title: 'Read Me', userId: 'UserId' },
+  { id: 7, title: 'Blibbity Bloo', userId: 'UserId' },
+  { id: 8, title: 'Favorite Food', userId: 'UserId' },
+  { id: 9, title: "Movies I've watched", userId: 'UserId' },
+  { id: 10, title: 'Favorite Animals', userId: 'UserId' },
+  { id: 11, title: 'Nostalgic TV Shows', userId: 'UserId' },
 ];
 
 export default mockJournals;
