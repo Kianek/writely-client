a7c35027cb62d3b5ebd97707e6655df454fdcfbb
Kianek <kallridge92@gmail.com>
2020-10-18 08:59:02 -0700

    Add reducers & action creators

--- src/data/users/usersSlice.js
+++ src/data/users/usersSlice.js
@@ -1,16 +1,59 @@
-import { createSlice } from '@reduxjs/toolkit';
+import status from '../status';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../config';
+
+export const signInAsync = createAsyncThunk(
+  'users/signIn',
+  async ({ email, password, rememberMe }, thunkAPI) => {
+    const response = await axios.post('/auth/login', {
+      email,
+      password,
+      rememberMe,
+    });
+    return response.data;
+  }
+);
+
+export const signOutAsync = createAsyncThunk('users/signOut', async () => {
+  try {
+    await axios.get('/auth/logout');
+  } catch (err) {}
+});
 
 export const usersSlice = createSlice({
   name: 'users',
-  initialState: { signedIn: false, user: {} },
+  initialState: {
+    signedIn: false,
+    user: null,
+    status: status.idle,
+    error: null,
+  },
   reducers: {
-    signIn: (state, action) => {
+    usersLoading(state, action) {
+      if (state.status === status.idle) {
+        state.status = status.loading;
+      }
+    },
+  },
+  extraReducers: {
+    [signInAsync.pending]: (state, action) => {
+      state.status = status.loading;
+    },
+    [signInAsync.fulfilled]: (state, action) => {
       state.signedIn = true;
       state.user = action.payload;
+      state.status = status.idle;
     },
-    signOut: (state) => (state.signedIn = false),
+    [signInAsync.rejected]: (state, action) => {
+      state.status = status.failed;
+    },
+    [signOutAsync.fulfilled]: (state, action) => {
+      state.user = null;
+      state.signedIn = false;
+    },
   },
 });
 
-export const { signIn, signOut } = usersSlice.actions;
+export const { usersLoading, signIn, signOut } = usersSlice.actions;
+
 export default usersSlice.reducer;
