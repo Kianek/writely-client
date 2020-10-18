3486f7e694a6ed88ed2e5b9dfacf02ea69501786
Kianek <kallridge92@gmail.com>
2020-10-18 08:52:03 -0700

    Hook up to redux

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,14 +2,17 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 import { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Panel from '../components/Panel';
 import Form from '../components/Form';
 import Button from '../components/Button';
+import Paragraph from '../components/Paragraph';
 import TextInput from '../components/TextInput';
 import HorizontalRule from '../components/HorizontalRule';
 import { isEmail, isValid, isEmpty } from '../validation';
 import useField from '../hooks/useField';
+import { useDispatch } from 'react-redux';
+import { signInAsync } from '../data/users/usersSlice';
 
 const linkStyles = (theme) => css`
   color: ${theme.colors.darkGray};
@@ -22,9 +25,13 @@ const linkStyles = (theme) => css`
 `;
 
 function SignIn() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [authError, setAuthError] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
   const [email, setEmail, emailError, setEmailError] = useField();
   const [password, setPassword, passwordError, setPasswordError] = useField();
+  const [loading, setLoading] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -35,9 +42,16 @@ function SignIn() {
       return;
     }
 
-    setEmail('');
-    setPassword('');
-    setRememberMe(false);
+    setLoading(true);
+    dispatch(signInAsync({ email, password, rememberMe }))
+      .then(() => {
+        setLoading(false);
+        setEmail('');
+        setPassword('');
+        setRememberMe(false);
+        history.replace('/dashboard');
+      })
+      .catch(() => setAuthError('Invalid email or password'));
   }
 
   return (
@@ -80,9 +94,20 @@ function SignIn() {
               onChange={() => setRememberMe(!rememberMe)}
             />
           </div>
-          <Button block outline onClick={handleSubmit}>
+          <Button
+            block
+            outline
+            disabled={loading}
+            loading={loading}
+            onClick={handleSubmit}
+          >
             Sign In
           </Button>
+          {authError && (
+            <Paragraph danger small>
+              {authError}
+            </Paragraph>
+          )}
         </Form>
         <HorizontalRule />
         <Link css={linkStyles} to="/register">
