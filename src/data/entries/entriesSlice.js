4d59b8bca417ec15b081702ab7d61746db794cde
Kianek <kallridge92@gmail.com>
2020-10-23 10:46:00 -0700

    Add actions; fill in reducers

--- src/data/entries/entriesSlice.js
+++ src/data/entries/entriesSlice.js
@@ -1,23 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import status from '../status';
+import { createSlice, createAction, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const selectEntry = createAction('entries/selectEntry', ({ id }) => {
+  return { payload: { id: id } };
+});
+
+export const loadEntries = createAction(
+  'entries/loadEntries',
+  ({ entries }) => {
+    return { payload: entries };
+  }
+);
 
 export const entriesSlice = createSlice({
   name: 'entries',
-  initialState: { entries: [], selectedEntry: {} },
+  initialState: {
+    entries: [],
+    selectedEntry: {},
+    status: status.idle,
+    error: null,
+  },
   reducers: {
-    select: (state, action) => {
-      state.selectedEntry = state.entries.find(action.payload.id);
+    selectEntry(state, action) {
+      state.selectedEntry = state.entries.find(
+        (entry) => entry.id === action.payload.id
+      );
     },
-    add: (state, action) => {
-      state.entries.push(action.payload);
+    loadEntries(state, action) {
+      state.entries = action.payload;
+      const entry = action.payload[0];
+      // If list is not empty, select the first entry
+      state.selectedEntry = entry ? entry : {};
     },
-    update: (state, action) => {
-      state.entries = state.entries.map((entry) => {
-        if (entry.id === action.payload.id) {
-          entry = action.payload;
-        }
-      });
+    addEntry(state, action) {
+      state.entries.shift(action.payload);
     },
-    remove: (state, action) => {
+    updateEntry(state, action) {
+      const { id, title, body } = action.payload;
+      const entry = state.entries.find(id);
+      if (entry) {
+        entry.title = title;
+        entry.body = body;
+      }
+    },
+    removeEntry(state, action) {
       state.entries = state.entries.filter(
         (entry) => entry.id !== action.payload
       );
@@ -25,5 +51,5 @@ export const entriesSlice = createSlice({
   },
 });
 
-export const { add, update, remove } = entriesSlice.actions;
+export const { addEntry, updateEntry, removeEntry } = entriesSlice.actions;
 export default entriesSlice.reducer;
