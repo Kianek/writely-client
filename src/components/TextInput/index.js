b69dca1ff4c912a0940b5e1f70b325663c30afce
Kianek <kallridge92@gmail.com>
2021-05-19 13:09:44 -0700

    Add validation

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,23 +1,49 @@
 import PropTypes from 'prop-types';
+import { Fragment, useState } from 'react';
 import './text-input.scss';
 
-function TextInput({ onChange, placeholder, value }) {
+function TextInput({ onChange, placeholder, validators, value }) {
+  const [errors, setErrors] = useState('');
+
   const setState = (event) => {
     onChange(event.target.value);
+    validate();
   };
+
+  const validate = () => {
+    if (!validators) {
+      return;
+    }
+
+    let errs = [];
+    validators.forEach((validator) => {
+      let result = validator(value);
+      if (typeof result === 'string') {
+        console.log(typeof result);
+        errs.push(result);
+      }
+    });
+
+    setErrors(errs.join(', '));
+  };
+
   return (
-    <input
-      type="text"
-      onChange={setState}
-      placeholder={placeholder}
-      value={value}
-    />
+    <Fragment>
+      <input
+        type="text"
+        onChange={setState}
+        placeholder={placeholder}
+        value={value}
+      />
+      {errors.length > 0 && <p data-testid="errors">{errors}</p>}
+    </Fragment>
   );
 }
 
 TextInput.propTypes = {
   onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string.isRequired,
+  validators: PropTypes.arrayOf(PropTypes.func),
   value: PropTypes.string.isRequired,
 };
 
