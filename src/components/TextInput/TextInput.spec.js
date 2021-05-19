ea8ac275e18b7fa1c4f5f219b20cad5cb9da84dc
Kianek <kallridge92@gmail.com>
2021-05-19 11:56:12 -0700

    Add event handler test

--- src/components/TextInput/TextInput.spec.js
+++ src/components/TextInput/TextInput.spec.js
@@ -1,10 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import TextInput from '.';
+import userEvent from '@testing-library/user-event';
 
 describe('TextInput', () => {
   test('renders successfully', () => {
     render(<TextInput placeholder="Email" value="" />);
 
     expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
+  });
+
+  test('calls event handler on typing', () => {
+    const jimEmail = 'jim@gmail.com';
+    let email = '';
+    let eventHandler = jest.fn((value) => {
+      email += value;
+    });
+    render(
+      <TextInput placeholder="Email" onChange={eventHandler} value={email} />
+    );
+
+    userEvent.type(screen.getByPlaceholderText('Email'), jimEmail);
+
+    expect(eventHandler).toBeCalledTimes(jimEmail.length);
+    expect(email).toBe(jimEmail);
   });
 });
