49b98003ec53977eedc211ddf58b97d1192bb332
Kianek <kallridge92@gmail.com>
2021-05-20 08:07:03 -0700

    Finish core markup

--- src/pages/LoginPortal.js
+++ src/pages/LoginPortal.js
@@ -1,23 +1,45 @@
+import { useState } from 'react';
+import { isEmail, isEmpty } from '../validators/validators';
 import Button from '../components/Button';
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextInput from '../components/TextInput';
+import Rule from '../components/Rule';
+import NavLink from '../components/NavLink';
 
 function LoginPortal(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const onSubmit = (e) => {
-    e.preventDefault();
     console.log('submitting');
   };
 
   return (
-    <div>
-      <Panel>
-        <h1>Login Portal</h1>
-        <Form onSubmit={onSubmit}>
-          <Button submit>Click me</Button>
-        </Form>
-      </Panel>
-    </div>
+    <Panel>
+      <h1>Sign In</h1>
+      <Form>
+        <TextInput
+          placeholder="Email"
+          onChange={setEmail}
+          validators={[isEmail]}
+          value={email}
+        />
+        <TextInput
+          password
+          placeholder="Password"
+          onChange={setPassword}
+          validators={[isEmpty]}
+          value={password}
+        />
+        <Button block submit onClick={onSubmit}>
+          Sign In
+        </Button>
+      </Form>
+      <NavLink to="/reset-password">Forgot your password?</NavLink>
+      <Rule />
+      <NavLink to="/register">No account? Register here.</NavLink>
+    </Panel>
   );
 }
 
