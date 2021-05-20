1898dfacde0255ea2e6a185e0ae6057329554b35
Kianek <kallridge92@gmail.com>
2021-05-19 18:25:47 -0700

    Use updated App.scss

--- src/App.js
+++ src/App.js
@@ -6,29 +6,33 @@ import Dashboard from './pages/Dashboard';
 import JournalView from './pages/JournalView';
 import Registration from './pages/Registration';
 import Settings from './pages/Settings';
+import Page from './components/Page';
+import './App.scss';
 
 function App() {
   return (
     <Router>
-      <div data-testid="App">
+      <div data-testid="App" className="app">
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <LoginPortal />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/journal/:id">
-            <JournalView />
-          </Route>
-          <Route path="/register">
-            <Registration />
-          </Route>
-          <Route path="/settings">
-            <Settings />
-          </Route>
-        </Switch>
+        <Page>
+          <Switch>
+            <Route exact path="/">
+              <LoginPortal />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/journal/:id">
+              <JournalView />
+            </Route>
+            <Route path="/register">
+              <Registration />
+            </Route>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+          </Switch>
+        </Page>
         <Footer />
       </div>
     </Router>
