dca8fec51a9cbccf94a4a5f170d87f45bf493a88
Kianek <kallridge92@gmail.com>
2021-05-20 18:19:20 -0700

    Add useHistory

--- src/pages/LoginPortal.js
+++ src/pages/LoginPortal.js
@@ -6,40 +6,45 @@ import Panel from '../components/Panel';
 import TextInput from '../components/TextInput';
 import Rule from '../components/Rule';
 import NavLink from '../components/NavLink';
+import { useHistory } from 'react-router';
 
 function LoginPortal(props) {
+  const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const onSubmit = (e) => {
     console.log('submitting');
+    history.push('/dashboard');
   };
 
   return (
-    <Panel>
-      <h1>Sign In</h1>
-      <Form>
-        <TextInput
-          placeholder="Email"
-          onChange={setEmail}
-          validators={[isEmail]}
-          value={email}
-        />
-        <TextInput
-          password
-          placeholder="Password"
-          onChange={setPassword}
-          validators={[isEmpty]}
-          value={password}
-        />
-        <Button block submit onClick={onSubmit}>
-          Sign In
-        </Button>
-      </Form>
-      <NavLink to="/reset-password">Forgot your password?</NavLink>
-      <Rule />
-      <NavLink to="/register">No account? Register here.</NavLink>
-    </Panel>
+    <main>
+      <Panel>
+        <h1>Sign In</h1>
+        <Form>
+          <TextInput
+            placeholder="Email"
+            onChange={setEmail}
+            validators={[isEmail]}
+            value={email}
+          />
+          <TextInput
+            password
+            placeholder="Password"
+            onChange={setPassword}
+            validators={[isEmpty]}
+            value={password}
+          />
+          <Button block submit onClick={onSubmit}>
+            Sign In
+          </Button>
+        </Form>
+        <NavLink to="/reset-password">Forgot your password?</NavLink>
+        <Rule />
+        <NavLink to="/register">No account? Register here.</NavLink>
+      </Panel>
+    </main>
   );
 }
 
