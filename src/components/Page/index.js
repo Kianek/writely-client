af2f35e8615ea2a2e34a5519fd7f78a27a8de1fe
Kianek <kallridge92@gmail.com>
2021-05-22 12:39:54 -0700

    Add id & testid

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,7 +1,11 @@
 import './page.scss';
 
 function Page({ children }) {
-  return <main>{children}</main>;
+  return (
+    <main id="page" data-testid="page">
+      {children}
+    </main>
+  );
 }
 
 export default Page;
