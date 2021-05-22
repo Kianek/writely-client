73faaf17a42802bd09c29f58b40eae01655330bd
Kianek <kallridge92@gmail.com>
2021-05-22 08:02:53 -0700

    Add `invokeSequence` function

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,23 @@ export const capitalize = (value) => {
 
   return value[0].toLocaleUpperCase() + value.slice(1);
 };
+
+export const invokeSequence = (
+  funcArray,
+  opts = { returnValues: false, filter: null }
+) => {
+  if (!funcArray || funcArray.length === 0) {
+    return;
+  }
+
+  if (opts.returnValues) {
+    let values = funcArray.map((func) => func());
+    if (!opts.filter) {
+      return values;
+    }
+
+    return values.filter((value) => typeof value === opts.filter);
+  }
+
+  funcArray.forEach((func) => func());
+};
