b31b843509568afd69fa002a595b4fa3603c0d90
Kianek <kallridge92@gmail.com>
2021-05-22 12:04:35 -0700

    Add directional props

--- src/components/ToolBar/index.js
+++ src/components/ToolBar/index.js
@@ -1,12 +1,23 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import './toolbar.scss';
 
-function ToolBar({ children }) {
-  return <div data-testid="toolbar">{children}</div>;
+function ToolBar({ children, left, right }) {
+  return (
+    <div
+      id="toolbar"
+      className={classNames({ left, right })}
+      data-testid="toolbar"
+    >
+      {children}
+    </div>
+  );
 }
 
 ToolBar.propTypes = {
   children: PropTypes.any.isRequired,
+  left: PropTypes.bool,
+  right: PropTypes.bool,
 };
 
 export default ToolBar;
