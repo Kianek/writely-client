a5739c12f26538667005a1a40f5240e7598ca6b6
Kianek <kallridge92@gmail.com>
2021-05-22 22:25:34 -0700

    Add className prop

--- src/components/ToolBar/index.js
+++ src/components/ToolBar/index.js
@@ -2,11 +2,11 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import './toolbar.scss';
 
-function ToolBar({ children, left, right }) {
+function ToolBar({ children, className, left, right }) {
   return (
     <div
       id="toolbar"
-      className={classNames({ left, right })}
+      className={classNames({ left, right }, className)}
       data-testid="toolbar"
     >
       {children}
@@ -16,6 +16,7 @@ function ToolBar({ children, left, right }) {
 
 ToolBar.propTypes = {
   children: PropTypes.any.isRequired,
+  className: PropTypes.string,
   left: PropTypes.bool,
   right: PropTypes.bool,
 };
