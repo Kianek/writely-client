d07eebfa784c8b09a3f8af059ace25af0a3d64eb
Kianek <kallridge92@gmail.com>
2021-05-23 10:34:14 -0700

    Add modal

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,34 @@
+import { Fragment, useState } from 'react';
 import { useRouteMatch } from 'react-router';
 import Button from '../components/Button';
+import Modal from '../components/Modal';
 import Panel from '../components/Panel';
 import ToolBar from '../components/ToolBar';
 import useNavigateTo from '../hooks/useNavigateTo';
 
 function Profile() {
+  const [showModal, setShowModal] = useState(false);
   const { url } = useRouteMatch();
   const func = () => console.log('click');
 
+  const onConfirm = () => {
+    setShowModal(false);
+  };
+
+  const onCancel = () => {
+    setShowModal(false);
+  };
+
   return (
-    <main>
+    <Fragment>
+      {showModal && (
+        <Modal
+          heading="Delete Account"
+          message="Are you sure? This cannot be undone."
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+        ></Modal>
+      )}
       <Panel>
         <ToolBar left>
           <Button info onClick={useNavigateTo('/dashboard')}>
@@ -24,11 +43,11 @@ function Profile() {
           Change Password
         </Button>
         <h3>Welcome to the Danger Zone</h3>
-        <Button block danger onClick={func}>
+        <Button block danger onClick={() => setShowModal(true)}>
           Delete Account
         </Button>
       </Panel>
-    </main>
+    </Fragment>
   );
 }
 
