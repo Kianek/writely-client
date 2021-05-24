075192de574048a989b56a62cb369ba4232bd256
Kianek <kallridge92@gmail.com>
2021-05-24 12:09:28 -0700

    Add classNames

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -1,9 +1,11 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import './text-area.scss';
 
-function TextArea({ placeholder, onChange, value }) {
+function TextArea({ fluid, placeholder, onChange, value }) {
   return (
     <textarea
+      className={classNames({ fluid })}
       placeholder={placeholder}
       onChange={onChange}
       value={value}
