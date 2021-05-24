d7b87d74f0cc4bffa887d0dba96abc1ce859e41b
Kianek <kallridge92@gmail.com>
2021-05-24 12:01:57 -0700

    Add to markup and type checking

--- src/components/EntryItem/index.js
+++ src/components/EntryItem/index.js
@@ -1,16 +1,21 @@
 import PropTypes from 'prop-types';
 import './entry-item.scss';
 
-function EntryItem({ title }) {
+function EntryItem({ entry, onClick }) {
   return (
-    <li>
-      <h2>{title}</h2>
+    <li id="entry-item" onClick={() => console.log(entry.id)}>
+      <h2>{entry.title}</h2>
     </li>
   );
 }
 
 EntryItem.propTypes = {
-  title: PropTypes.string,
+  entry: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+  }),
 };
 
 export default EntryItem;
