93123cb75ce229fb071811b35b857e1b83076bd3
Kianek <kallridge92@gmail.com>
2021-05-28 21:00:05 -0700

    Integrate TextInputUnderline

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -1,15 +1,24 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import TextInputUnderline from '../TextInputUnderline';
 import './text-area.scss';
 
 function TextArea({ fluid, placeholder, onChange, value }) {
+  const [hasFocus, setHasFocus] = useState(null);
+
   return (
-    <textarea
-      className={classNames({ fluid })}
-      placeholder={placeholder}
-      onChange={onChange}
-      value={value}
-    ></textarea>
+    <div id="text-area">
+      <textarea
+        className={classNames({ fluid })}
+        placeholder={placeholder}
+        onChange={onChange}
+        onFocus={() => setHasFocus(true)}
+        onBlur={() => setHasFocus(false)}
+        value={value}
+      ></textarea>
+      <TextInputUnderline hasFocus={hasFocus} posY={-5} />
+    </div>
   );
 }
 
