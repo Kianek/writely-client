de8575d36b9212e7a0bb77417f2e068775bd4a8d
Kianek <kallridge92@gmail.com>
2021-06-01 10:23:55 -0700

    Add Column component

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './panel.scss';
+import Column from '../Column';
 
 function Panel({ wide, children }) {
   return (
     <div className={classNames('panel', { wide })} data-testid="panel">
-      {children}
+      <Column>{children}</Column>
     </div>
   );
 }
