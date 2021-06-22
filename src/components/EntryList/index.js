09f667b9b7a2a096d7e06ed4209c2f902f620e15
Kianek <kallridge92@gmail.com>
2021-06-22 09:01:29 -0700

    Rework markup and add null entries checking

--- src/components/EntryList/index.js
+++ src/components/EntryList/index.js
@@ -4,13 +4,19 @@ import EntryItem from '../EntryItem';
 import List from '../List';
 import './entry-list.scss';
 
-function EntryList({ className, entries }) {
+function EntryList({ className, entries, height }) {
+  if (!entries) {
+    return <p>Error loading entries</p>;
+  }
+
   return (
     <section id="entry-list" data-testid="entry-list">
-      <List borderless thin>
-        {entries.map((entry) => (
-          <EntryItem key={entry.id} entry={entry} />
-        ))}
+      <List height={height}>
+        {entries.length > 0 ? (
+          entries.map((entry) => <EntryItem key={entry.id} entry={entry} />)
+        ) : (
+          <p>No entries yet.</p>
+        )}
       </List>
     </section>
   );
