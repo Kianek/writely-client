f5ac530e9aa8fd82161283c2b25dce2fed049b18
Kianek <kallridge92@gmail.com>
2021-06-23 11:47:33 -0700

    Add height prop and style

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,14 +2,18 @@ import classNames from 'classnames';
 
 import './list.scss';
 
-function List({ borderless, className, children, fluid, thin }) {
+function List({ borderless, className, children, fluid, height, thin }) {
   const classes = {
     borderless,
     fluid,
     thin,
   };
   return (
-    <ul className={classNames('list', classes, className)} data-testid="list">
+    <ul
+      className={classNames('list', classes, className)}
+      data-testid="list"
+      style={{ height: height ? height : '400px' }}
+    >
       {children}
     </ul>
   );
