14de42c73f3e55b6ec72505ec8e96b0234ccccd9
Kianek <kallridge92@gmail.com>
2021-07-05 18:57:13 -0700

    Refactor mocks

--- src/store/journals/mock-journals.js
+++ src/store/journals/mock-journals.js
@@ -1,80 +1,34 @@
-let entryId = 0;
+import faker from 'faker';
+
 function getEntries(journalId, numOfEntries) {
+  let entryId = 0;
   let entries = [];
-  for (let i = 0; i < numOfEntries; i++, entryId++) {
+  for (let i = 1; i <= numOfEntries; i++, entryId++) {
     entries.push({
       journalId,
       id: entryId,
       title: `Entry ${entryId}`,
       tags: 'one,two',
       body: 'Blah dee bloo',
+      lastModified: faker.date.past().toDateString(),
     });
   }
 
   return entries;
 }
 
-const journals = [
-  {
-    id: 1,
-    title: 'Spiffy Journal',
-    entries: getEntries(1, 4),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 2,
-    title: 'Dreams',
-    entries: getEntries(2, 2),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 3,
-    title: 'Favorite Movies',
-    entries: getEntries(3, 15),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 4,
-    title: 'Favorite Movies',
-    entries: getEntries(4, 15),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 5,
-    title: 'Favorite Movies',
-    entries: getEntries(5, 15),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 6,
-    title: 'Favorite Movies',
-    entries: getEntries(6, 15),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 7,
-    title: 'Favorite Movies',
-    entries: getEntries(7, 15),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 8,
-    title: 'Favorite Movies',
-    entries: getEntries(8, 15),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 9,
-    title: 'Favorite Movies',
-    entries: getEntries(9, 15),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-  {
-    id: 10,
-    title: 'Favorite Movies',
-    entries: getEntries(10, 15),
-    createdAt: new Date().toLocaleTimeString(),
-  },
-];
+function getJournals(numOfJournals) {
+  let journals = [];
+  for (let i = 1; i <= numOfJournals; i++) {
+    journals.push({
+      id: i,
+      title: faker.random.words(),
+      entries: getEntries(i, Math.floor(Math.random() * 10 + 1)),
+      createdAt: faker.date.past(0).toDateString(),
+    });
+  }
 
-export default journals;
+  return journals;
+}
+
+export default getJournals(5);
